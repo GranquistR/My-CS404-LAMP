972595e0655d9d05df1034606031cc486d983b6a
Ryan Granquist <rmg61103@gmail.com>
2024-02-07 18:28:03 -0600

    Moved to mvc pattern

--- www/CS404/Module1.1/movieBrowse.php
+++ www/CS404/Module1.1/movieBrowse.php
@@ -1,93 +1,44 @@
-<!-- url: http://localhost:3000/CS404/Module1.1/movieBrowse.php -->
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Module 1.1 - Movie Browser</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-
-    <style>
-    .movieSummary {
-      border: 1px solid lightgrey;
-      border-radius: 10px;
-      box-shadow: 3px 3px 6px gray;
-      transition: box-shadow 0.3s ease-in-out;
-      text-align: center;
-      padding: 5px;
-      margin-bottom: 15px;
-      vertical-align: bottom;
-    }
-    .summaryTitle { display: block; font-size: medium; height: 4.5em; }
-    .summaryInfo { display: block; font-size: medium; height: 5em; }
-    </style>
-  </head>
-  <body>
-    <div class='container'>
-      <div class='row'>
-        <div class="pb-2 mt-4 mb-2 border-bottom" style="width: 100%;">
-          <h1>MyFlix Movie Browser</h1>
-          Click on a movie below for more information.
-        </div>
-      </div>
 <?php
+// Set a json content type
+header('Content-Type: application/json');
+
 // Establish the database connection
-include "../../database.php"; // TODO: <-- Don't forget to update this file with your username and password
+include "../../database.php"; 
 $db = connectToDatabase("myFlix");
 if($db == NULL) {
-  die("<p>Failed to connect to database.</p></body></html>\n");
+  http_response_code(500);
+  $message = array("error"=>TRUE, "message"=>"Failed to connect to database.");
+  die(json_encode($message));
 }
 
 // Prepare and execute a query for the basic movie information
-$stmt = simpleQuery($db, "SELECT id, title, year, genres, image, imdbrating FROM movies;");
+$stmt = simpleQuery($db, "SELECT id, title, year, genres, image, rated FROM movies;", FALSE);
 if($stmt == NULL) {
-  die("<p>SQL Query Error</p></body></html>\n");
+  http_response_code(500);
+  $message = array("error"=>TRUE, "message"=>"SQL Query Error");
+  die(json_encode($message));
 }
 
-// Bind variables to the results (same order as in the query)
-$stmt->bind_result($movieID, $movieName, $movieYear, $movieGenre, $movieImage, $movieRating);
+// Get the result set
+$result_set = $stmt->get_result();
+if ($result_set == NULL) {
+  http_response_code(500);
+  $message = array("error"=>TRUE, "message"=>"Failed to get result set.");
+  die(json_encode($message));
+}
 
-//Process the resutls and output in bootstrap grid form
-echo "      <div class='row'>\n";
+// Get all the rows from the result set
+$movieData = $result_set->fetch_all(MYSQLI_ASSOC);
+if ($movieData == NULL) {
+  http_response_code(500);
+  $message = array("error"=>TRUE, "message"=>"Failed to fetch all rows.");
+  die(json_encode($message));
+}
 
-while($stmt->fetch()) {
-  // Output a grid cell for the current movie
-?>
-        <div class='col-sm-6 col-md-4 col-lg-3'>
-          <div class='movieSummary' onmouseover='overCard(this)' onmouseout='outCard(this)'>
-            <a href='movieDetails.php?movieID=<?=$movieID?>'>
-              <span class="summaryTitle"><?=$movieName?></span>
-              <img src="../../../thumbs/<?=$movieImage?>" alt="<?=$movieName?>" height="250px">
-            </a><br>
-            <span class="summaryInfo"><?=$movieGenre?><br><?=$movieYear?>, <?=$movieRating?>/10</span>
-            
-          </div> <!-- /movieSummary-->
-        </div> <!-- /col* -->
-<?php
-} //end of while loop
-
-echo "      </div> <!-- /row -->\n";
+// Return the movie data as a JSON object
+echo json_encode($movieData);
 
 // Close the database connection
 $stmt->close();
 $db->close();
 ?>
-    </div> <!-- /container -->
-
-    <!-- Custom JS -->
-    <script>
-      function overCard(card) {
-        card.style.boxShadow = "5px 5px 10px gray";
-      }
-
-      function outCard(card) {
-        card.style.boxShadow = "3px 3px 6px gray";
-      }
-    </script>
-
-    <!-- Bootstrap JS (with popper integerated) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  </body>
-</html>
--- www/CS404/Module1.1/movieDetails.php
+++ www/CS404/Module1.1/movieDetails.php
@@ -1,76 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <title>Module 1.1 - Movie Details</title>
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class='container'>
-      <div class='row'>      
-        <div class="pb-2 mt-4 mb-2 border-bottom">
-          <h1>MyFlix Movie Details</h1>
-        </div>
-      </div>
 <?php
+header('Content-Type: application/json');
+
 if(isset($_GET['movieID']))
 {
   // Get ID of movie from form GET data
   $movieID = $_GET['movieID'];
 
   // 1. Connect to the database
-  include "database.php"; // TODO: <-- Don't forget to update this file with your username and password
-  $db = connectToDatabase("** TODO: Fill in your DB name **");
+  include "database.php";
+  $db = connectToDatabase("myflix");
   if ($db->connect_error) {
-    die("<p>Failed to connect to database.</p></body></html>\n");
+    http_response_code(500);
+    $message = array("error"=>TRUE, "message"=>"Failed to connect to database.");
+    die(json_encode($message));  
   }
 
   // 2. Run the Query
   $query = "SELECT title,year,rated,imdbrating,description,image,genres,directors,writers,actors FROM movies WHERE id=?;";
-  $stmt = simpleQueryParam($db, $query, "s", $movieID);
+  $stmt = simpleQueryParam($db, $query, "s", $movieID, FALSE);
   if($stmt == NULL) {
-    die("<p>SQL Query Error</p></body></html>\n");
+    http_response_code(500);
+    $message = array("error"=>TRUE, "message"=>"SQL Query Error");
+    die(json_encode($message));
   }
 
-  // 3. Bind and access the result variables
-  if(!$stmt->bind_result($movieName, $movieYear, $movieRated, $movieIMDB, $movieDesc, $movieImage,
-    $movieGenres, $movieDirectors, $movieWriters, $movieActors)) {
-    die("<p>Query Result Binding Failed: " . $stmt->error . "</p></div></body></html>");
+  // Get the result set
+  $result_set = $stmt->get_result();
+  if ($result_set == NULL) {
+    http_response_code(500);
+    $message = array("error"=>TRUE, "message"=>"Failed to get result set.");
+    die(json_encode($message));
   }
 
-  // Fetch and display the results
-  if($stmt->fetch()) {
-    // TODO: Adjust the following to present all of this information in a more 
-    //   organized format. You must use an image tag to display the full resolution
-    //   poster, a table to organize all the data (or the Bootstrap column system),
-    //   and CSS to keep things looking styled similar to the movieBrowse page.
-    //   Feel free to use Bootstrap classes to achieve better styling.
-?>
-        <ul>
-          <li>ID = <?=$movieID?></li>
-          <li>Name = <?=$movieName?></li>
-          <li>Year = <?=$movieYear?></li>
-          <li>Rating = <?=$movieRated?></li>
-          <li>IMDB Score = <?=$movieIMDB?></li>
-          <li>Desc = <?=$movieDesc?></li>
-          <li>Image = <?=$movieImage?></li>
-          <li>Genres = <?=$movieGenres?></li>
-          <li>Directors = <?=$movieDirectors?></li>
-          <li>Writers = <?=$movieWriters?></li>
-          <li>Actors = <?=$movieActors?></li>
-        </ul>
-<?php
+  // Get the one movie from the result set
+  $movieData = $result_set->fetch_array(MYSQLI_ASSOC);
+  if ($movieData == NULL) {
+    http_response_code(404);
+    $message = array("error"=>TRUE, "message"=>"Movie not found.");
+    die(json_encode($message));
   }
+
+  // Output the movie data as a JSON object
+  echo json_encode($movieData);
+
 } else { // Not isset($_GET['movieID'])
-?>
-        <p>Error: no movie ID provided.</p>
-<?php
+  http_response_code(400);
+  $message = array("error"=>TRUE, "message"=>"Movie ID required.");
+  echo json_encode($message);
 }
 ?>
-    </div> <!-- /container -->
-
-    <!-- Bootstrap JS (with popper integerated) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-  </body>
-</html>
